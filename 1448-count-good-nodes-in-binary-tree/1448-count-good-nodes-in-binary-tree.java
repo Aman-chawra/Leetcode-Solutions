2c866e5cb3be46de60283e9cddf3274082d8e581
Aman Chawra <74526775+Aman-chawra@users.noreply.github.com>
2022-09-02 01:00:29 +0530

    Time: 3 ms (76.91%), Space: 59.6 MB (64.37%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
@@ -12,19 +12,6 @@
  *         this.right = right;
  *     }
  * }
-int good;
-    public int goodNodes(TreeNode root) {
-        good = 0;
-        cal(root, Integer.MIN_VALUE);
-        return good;
-    }
-    void cal(TreeNode root, int max){
-        if(root == null) return;
-        if(root.val >= max) good++;
-        max = Math.max(max, root.val);
-        cal(root.left, max);
-        cal(root.right, max);
-    }
  */
 class Solution {
     int good;
