5c8230005f3dfd33db82602750a3eadb3084c61a
Aman Chawra <74526775+Aman-chawra@users.noreply.github.com>
2022-09-08 11:25:54 +0530

    Time: 6 ms (53.06%), Space: 42.1 MB (98.96%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -1,6 +1,30 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        for(int i =0;i<nums1.length;i++)
+        for(int i=0;i<nums1.length;i++)
+        {
+            boolean flag1 = false;
+            boolean flag2 = false;
+            for(int j=0;j<nums2.length;j++)
+            {
+                if(flag1 || nums1[i] == nums2[j])
+                {
+                    flag1 = true;
+                    if(nums1[i]<nums2[j])
+                    {
+                        nums1[i] = nums2[j];
+                        flag2 = true;
+                        break;
+                    }
+                }
+            }
+            if(flag2 == false) nums1[i] = -1;
+        }
+        return nums1;
+    }
+}
+
+/*
+for(int i =0;i<nums1.length;i++)
         {
             boolean chk=false,chk1=false;
             for(int j=0;j<nums2.length;j++)
@@ -9,7 +33,7 @@ class Solution {
                 if(chk || nums1[i] == nums2[j])
                 {
                     chk=true;
-                    if(nums1[i]<nums2[j])
+                    if(nums1[i]<numsz2[j])
                     { 
                         nums1[i] = nums2[j];
                         chk1=true;
@@ -22,5 +46,4 @@ class Solution {
                 nums1[i]=-1;
         }
         return nums1;
-    }
-}
+*/
